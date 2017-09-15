580f1f4047b41884df685396b0323c44c311bec9
Test Admin <test.admin@jt-corp.jameslakin.co.uk>
2017-09-15 08:24:57 +0100

    Release onto Google Play

--- app/screens/settings.js
+++ app/screens/settings.js
@@ -21,6 +21,14 @@ export default class SettingsScreen extends Component {
         <ScrollView>
           <SettingsCredentials/>
           <SettingsAdFree/>
+          <Text>Privacy Policy:</Text>
+          <Text>PSC Companion does not transmit personal data to any servers outside of college.
+          Advertising (AdMob) is used in this application, however it may be switched off in the settings menu.
+          Credentials for the college account are not stored on the app by default, unless you have activated
+          the intranet auto login feature. This application requires the Phone State permission due the framework
+          which this app is built on, it does not make use of phone functionality.</Text>
+          <Text>About:</Text>
+          <Text>Version 0.1 Alpha, by James Lakin.</Text>
         </ScrollView>
       </View>
     );
