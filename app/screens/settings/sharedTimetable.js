682dc287802ac9376537c22b973a32d41d2fdf57
Test Admin <test.admin@jt-corp.jameslakin.co.uk>
2017-09-28 17:26:19 +0100

    Add PIN management

--- app/screens/settings/sharedTimetable.js
+++ app/screens/settings/sharedTimetable.js
@@ -61,6 +61,8 @@ export default class SettingSharedTimetable extends Component {
           in a database I host.</Text>
         {!this.state.enrolled && <Button onPress={this.enroll} title="Enroll" />}
         <Text>{JSON.stringify(this.state.pinAndKey)}</Text>
+        {this.state.enrolled &&
+          <Text style={{fontSize: 18}}>Your PIN is {this.state.pinAndKey.pin}</Text>}
         {this.state.enrolled && <SharedPinManager />}
       </View>
     );
@@ -97,24 +99,39 @@ class SharedPinManager extends Component {
   addPin() {
     var pins = this.state.savedPins;
     pins.push(this.state.newPin);
+    this.setState({savedPins: pins, newPin: null});
+    AsyncStorage.setItem('sharedSavedPins', JSON.stringify(pins));
+  }
+
+  removePin(pin) {
+    var pins = this.state.savedPins;
+    pins.splice(pins.indexOf(pin), 1);
     this.setState({savedPins: pins});
     AsyncStorage.setItem('sharedSavedPins', JSON.stringify(pins));
   }
 
-  removePin(index) {
-
-  }
-
   render() {
+    var self = this
     return (
       <View>
-        <View style={{flexDirection: "row", flex: 1}}>
+        <View style={{flexDirection: "row", justifyContent: "space-between"}}>
           <TextInput
             placeholder="Enter a 6 digit pin"
+            keyboardType="numeric"
+            defaultValue={this.state.newPin}
             onChangeText={(pin) => {this.setState({newPin: pin})}}
             style={{flex: 2}} />
           <Button title="Add" onPress={this.addPin} style={{flex: 1}} />
         </View>
+
+        {this.state.savedPins.map(function (pin) {
+          return (
+            <View key={pin} style={{flexDirection: "row", justifyContent: "space-between"}}>
+              <Text>{pin}</Text>
+              <Button title="Delet this" onPress={() => {self.removePin(pin)}} />
+            </View>
+          )
+        })}
       </View>
     );
   }
