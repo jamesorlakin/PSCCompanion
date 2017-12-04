c5d5d152fcfb4c7e77abe9e26c605019c393fda5
James Lakin <james@jameslakin.co.uk>
2017-12-04 19:20:03 +0000

    Set iOS status bar to white

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { Constants } from 'expo';
 
 import PSCCompanion from './app/index.js';
@@ -7,7 +7,12 @@ import PSCCompanion from './app/index.js';
 export default class App extends React.Component {
   render() {
     return (
-      <PSCCompanion style={{marginTop: Constants.statusBarHeight}} />
+      <View style={{flex: 1}}>
+        <StatusBar
+          backgroundColor="blue"
+          barStyle="light-content"/>
+        <PSCCompanion style={{marginTop: Constants.statusBarHeight}} />
+      </View>
     );
   }
 }
