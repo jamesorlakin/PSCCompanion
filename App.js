e208712662b7e6b74a958f027bbabf275dbd91d9
James Lakin <james@jameslakin.co.uk>
2017-12-04 09:43:34 +0000

    Attempt to fix statusbar margins.

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Constants } from 'expo';
 
 import PSCCompanion from './app/index.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <PSCCompanion />
+      <PSCCompanion style={{marginTop: Constants.statusBarHeight}} />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
