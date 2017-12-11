b5bb83048a3d51e6b74fde117ca8a0b46dc61d76
James Lakin <james@jameslakin.co.uk>
2017-12-11 13:36:33 +0000

    Add floating and clashing lessons to welcome.

--- app/summaryComponent.js
+++ app/summaryComponent.js
@@ -66,30 +66,68 @@ export default class Summary extends Component {
     }
 
     return (
-      <View style={styles.container}>
-        <View style={{flexDirection: 'row', justifyContent: "space-between"}}>
+      <View>
+        <View style={styles.container}>
 
-          <Text style={{fontWeight: 'bold'}}>What's next?</Text>
-          {this.state.data.isCached && <View style={{flexDirection: 'row'}}>
-            <Text style={{fontStyle: 'italic'}}>Updating </Text>
-            <ActivityIndicator />
-          </View>}
+          <View style={{flexDirection: 'row', justifyContent: "space-between"}}>
+            <Text style={{fontWeight: 'bold'}}>What's next?</Text>
+            {this.state.data.isCached && <View style={{flexDirection: 'row'}}>
+              <Text style={{fontStyle: 'italic'}}>Updating </Text>
+              <ActivityIndicator />
+            </View>}
+          </View>
+
+          <SummaryEvent event={nextEvent} />
         </View>
 
-
-        {nextEvent.type != "unknown" && <View style={{
-          backgroundColor: nextEvent.Color,
-          height: 3}}
-        />}
-        <Text>{nextEvent.Type != "unknown" && nextEvent.Title + " - "
-          + moment.unix(nextEvent.Start).fromNow()}</Text>
-        <Text>{nextEvent.Type != "unknown" && nextEvent.Staff}</Text>
-        <Text>{moment.unix(nextEvent.Start).format('dddd, HH:mm A') + " - "}
-          {moment.unix(nextEvent.End).format('HH:mm A')} : {nextEvent.Room}</Text>
-
+        {this.state.data.clashing.length > 0 &&
+          <ClashingLessons events={this.state.data.clashing} />}
+        {this.state.data.floating.length > 0 &&
+          <FloatingLessons events={this.state.data.floating} />}
       </View>
     );
   }
+}
+
+function ClashingLessons(props) {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontWeight: 'bold'}}>Clashing lessons:</Text>
+      {props.events.map(function (event) {
+        return <SummaryEvent event={event} key={event.Title+event.Start}/>
+      })}
+    </View>
+  )
+}
+
+function FloatingLessons(props) {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontWeight: 'bold'}}>Floating lessons:</Text>
+      <Text>Floating lessons don't have an exact time. However, the time given
+        should refer to the week of the lesson.
+      </Text>
+      {props.events.map(function (event) {
+        return <SummaryEvent event={event} key={event.Title+event.Start}/>
+      })}
+    </View>
+  )
+}
+
+function SummaryEvent(props) {
+  if (props.event.Type === "unknown") return (<Text>Unknown</Text>)
+  return (
+    <View>
+      <View style={{
+        backgroundColor: props.event.Color,
+        height: 3}}
+      />
+      <Text>{props.event.Title + " - " + moment.unix(props.event.Start).fromNow()}</Text>
+      <Text>{props.event.Staff}</Text>
+      <Text>{moment.unix(props.event.Start).format('dddd, HH:mm A') + " - "}
+        {moment.unix(props.event.End).format('HH:mm A')} : {props.event.Room}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
