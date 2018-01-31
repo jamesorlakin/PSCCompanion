6ce7879baa7e47f7aecb3eae4977bf8f75566539
James Lakin <james@jameslakin.co.uk>
2018-01-31 10:15:14 +0000

    Remove attendance screen from navigation. It's not useful.

--- app/indexHost.js
+++ app/indexHost.js
@@ -21,7 +21,7 @@ import RoomTimetableScreen from './screens/roomTimetable.js'
 import SharedTimetableScreen from './screens/sharedTimetable.js'
 import StudentNoticesScreen from './screens/notices.js'
 import MapScreen from './screens/map.js'
-import AttendanceScreen from './screens/attendance.js'
+//import AttendanceScreen from './screens/attendance.js'
 import IntranetScreen from './screens/intranet.js'
 import SettingsScreen from './screens/settings.js'
 import GrimeScreen from './screens/grime.js'
@@ -36,7 +36,7 @@ const WelcomeNavigator = StackNavigator({
 const Drawer = DrawerNavigator({
   welcome: {screen: WelcomeNavigator},
   freeroom: {screen: FreeRoomScreen},
-  attendance: {screen: AttendanceScreen},
+//  attendance: {screen: AttendanceScreen},
   userTimetable: {screen: UserTimetableScreen},
   roomTimetable: {screen: RoomTimetableScreen},
   sharedTimetable: {screen: SharedTimetableScreen},
